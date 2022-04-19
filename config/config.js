17619f8e5569c67e6b5ce33aa7212586cda4cc56
Dinesh Rathod <pr760677@gmail.com>
2022-04-19 18:44:36 +0530

    Database changed

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    /*   MongoUrl :'mongodb+srv://dinesh:RathodDinesh@dinesh.nqph4.mongodb.net/upload_img2?retryWrites=true&w=majority', */
-    url: "mongodb://localhost:27017/drustii",
+    url: process.env.DB_URL,
     model:"video"
 };
